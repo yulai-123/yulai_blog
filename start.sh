37d3cada8990990ed061609a8b5ce5eef651d4d4
yulai <thesky341@foxmail.com>
2025-10-18 21:15:08 +0800

    更新启动脚本为zsh并添加数据迁移配置文件

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/zsh
 
 git pull;
 
@@ -6,4 +6,4 @@ ps -ef | grep hexo | grep -v grep | awk '{print $2}' | xargs kill -9;
 
 hexo clean
 hexo generate
-nohup hexo server -p 4567 -s > logs/hexo.log 2>&1 &
+nohup hexo server -p 2321 -s > logs/hexo.log 2>&1 &
